c4ddaa85e17d60b83089d243e6020ceb54e3a632
Akiyama <avoqun@gmail.com>
2025-05-05 19:27:55 +0300

    updated pt2

--- src/utils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-char* safe_strdup(const char* str);
-size_t count_int_strlen(size_t n);
-size_t count_double_strlen(double value, size_t precision);
-void append_str(char **dest, const char *src);
